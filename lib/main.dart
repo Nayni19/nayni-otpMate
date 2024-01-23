53eaf1a5a9a9d45fb04941b310247db6be357a9b
nayni <nayni.shah@5paisa.com>
2024-01-23 12:20:00 +0530

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:otp_app/home/homepage.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
@@ -11,13 +17,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Otp Mate',
       theme: ThemeData(
-         textTheme: GoogleFonts.poppinsTextTheme(),
+        textTheme: GoogleFonts.poppinsTextTheme(),
         // colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFFf3ecf9),),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: AnimatedSplashScreen(
+        splash: 'images/splash.png',
+        duration: 3000,
+        animationDuration: const Duration(milliseconds: 2000),
+        nextScreen: HomePage(),
+        splashIconSize: 200,
+        backgroundColor: Color(0xffF1EDFF),
+        splashTransition: SplashTransition.scaleTransition,
+        pageTransitionType: PageTransitionType.rightToLeft,
+      ),
     );
   }
 }
